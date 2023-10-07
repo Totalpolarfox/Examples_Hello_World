c5d34265c0b9556bf61f85c0e0bee32c3c35ea0f
Totalpolarfox <totalpolarfox@outlook.com>
2023-10-07 14:26:27 +0300

    Examle 010 MethodoArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 15, 27, 33, 42, 55, 69, 71, 83, 55, 90 };
+int n = array.Length;                                   // присваиваем кол-во элементов массива
+int find = 55;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("Индекс: " + index);
+        Console.WriteLine("Число в нем: " + array[index]);
+        break;                          // прервать выполнение (найдено первое совпадение)
+    }
+    index++;            // index = index + 1
+}
