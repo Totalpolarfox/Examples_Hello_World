260c1c1431ae713e1b83690202bc2fba9a83ca3f
Totalpolarfox <totalpolarfox@outlook.com>
2023-10-07 15:41:04 +0300

    Examle 011 ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -25,5 +25,30 @@ void PrintArray(int[] col)
 
 int[] array = new int[10];      //создать новый массив, в котором будет 10 элементов
 
+
+// Находим позицию нужного элемента массива
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;  // получаем длину массива из int[] array = new int[10];
+    int index = 0;           // задаем начальную позицию в массиве
+    int posit = -1;          // -1 для обозначения в выводе не найденного числа
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            posit = index;
+            break;          // прервать выполнение (найдено первое совпадение)
+        }
+        index++;
+    }
+    return posit;
+}
+
+
+
 FillArray(array);
 PrintArray(array);
+Console.WriteLine(); // Вывод пустой строки, чтобы убедиться, что не принадлежит к предыдущим
+
+int pos = IndexOf(array, 4);
+Console.WriteLine("Позиция (индекс) заданного числа: "+pos);
